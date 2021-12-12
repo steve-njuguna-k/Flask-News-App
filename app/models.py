81f18c1e9baac516d8b042cdf6ab42da4b0d713d
Steve Njuguna <stevenjuguna93@gmail.com>
2021-12-12 23:01:24 +0300

    Added models.py

---
+++ app/models.py
@@ -0,0 +1,23 @@
+import string
+
+class Sources:
+    def __init__(self,id:string,name,description,url,country,language):
+        self.id=id,
+        self.name=name,
+        self.description=description
+        self.url=url
+        self.language=language
+        self.country=country
+        
+class Articles:
+    '''Define article model'''
+    def __init__(self,source,author,title,description,url,urlToImage,publishedAt,content):
+        self.source=source
+        self.author=author
+        self.title=title
+        self.description=description
+        self.url=url
+        self.urlToImage=urlToImage
+        self.publishedAt=publishedAt
+        self.content=content
+
