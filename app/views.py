adf04a6697bcdd813bd21c9f283403c6aa2fe236
Steve Njuguna <stevenjuguna93@gmail.com>
2021-12-12 20:30:40 +0300

    Made changes to request.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,34 @@
 from app import app
 from flask import render_template
+from newsapi import NewsApiClient
+from .config import API_KEY
 
 @app.route('/')
 def home():
-    return  render_template('home.html')
+
+    newsapi = NewsApiClient(api_key= API_KEY)
+
+    top_headlines = newsapi.get_top_headlines(sources= 'techcrunch')
+
+    all_articles = top_headlines['articles']
+
+    title = []
+    desc = []
+    author = []
+    img = []
+    p_date = []
+    url = []
+
+    for i in range(len(all_articles)):
+        main_article = all_articles[i]
+
+        title.append(main_article['title'])
+        desc.append(main_article['description'])
+        author.append(main_article['author'])
+        img.append(main_article['urlToImage'])
+        p_date.append(main_article['publishedAt'])
+        url.append(main_article['url'])
+
+        content = zip(title, desc, author, img, p_date, url)
+
+    return  render_template('home.html', content = content)
