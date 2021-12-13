1f54532300e361b58ca841354e58bc6e68360ea8
Steve Njuguna <stevenjuguna93@gmail.com>
2021-12-13 21:08:35 +0300

    Added topHeadlines function

--- app/request.py
+++ app/request.py
@@ -44,3 +44,39 @@ def publishedArticles():
         contents = zip(source, title, desc, author, img, p_date, url)
 
     return  contents
+
+def topHeadlines():
+    newsapi = NewsApiClient(api_key= Config.API_KEY)
+
+    top_headlines = newsapi.get_top_headlines(sources= 'cnn, reuters, cnbc, techcrunch, the-verge, gizmodo, the-next-web, techradar, recode, ars-technica')
+
+    all_articles = top_headlines['articles']
+
+    articles_results = []
+
+    source = []
+    title = []
+    desc = []
+    author = []
+    img = []
+    p_date = []
+    url = []
+
+    for i in range(len(all_articles)):
+        main_article = all_articles[i]
+
+        source.append(main_article['source'])
+        title.append(main_article['title'])
+        desc.append(main_article['description'])
+        author.append(main_article['author'])
+        img.append(main_article['urlToImage'])
+        p_date.append(main_article['publishedAt'])
+        url.append(main_article['url'])
+
+        article_object = Articles(source, title, desc, author, img, p_date, url)
+
+        articles_results.append(article_object)
+
+        contents = zip(source, title, desc, author, img, p_date, url)
+
+    return  contents
