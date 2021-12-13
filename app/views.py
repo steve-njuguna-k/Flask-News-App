4fc62a1cbfa9d3816c2aaeb993dde485e32fd35f
Steve Njuguna <stevenjuguna93@gmail.com>
2021-12-13 21:14:38 +0300

    Added headlines page

--- app/views.py
+++ app/views.py
@@ -1,10 +1,15 @@
 from app import app
 from flask import render_template
-from .request import publishedArticles
-
+from .request import publishedArticles, topHeadlines
 
 @app.route('/')
 def home():
     articles = publishedArticles()
 
     return  render_template('home.html', articles = articles)
+
+@app.route('/headlines')
+def headlines():
+    headlines = topHeadlines()
+
+    return  render_template('headlines.html', headlines = headlines)
