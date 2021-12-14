a348b2b625e82a8db4f0531e7e2b7e0c4f11faaf
Steve Njuguna <stevenjuguna93@gmail.com>
2021-12-14 13:40:37 +0300

    Made changes to config.py

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Config:
     NEWS_BASE_EVERYTHING_URL = 'https://newsapi.org/v2/everything?domains={}&apiKey={}'
     NEWS_BASE_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
     NEWS_BASE_SOURCE = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    API_KEY = "2c90171bfd444518bc4ebda2e5895ba0"
+    API_KEY = "f1a683b7df544ace8de3d9ce54790eb1"
 
 class ProdConfig(Config):
     pass
